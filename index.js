a25f846c63d7ca4e30be8cd749d754274d0a62b4
Marcio Oliveira <marciotojr@gmail.com>
2023-10-05 23:57:51 -0300

    Add prettier and eslint

--- index.js
+++ index.js
@@ -10,27 +10,27 @@ const url = process.argv[2];
 const css = fs.readFileSync('./css/style.css');
 
 const request = http.request(url, (res) => {
-  let data = '';
-  res.on('data', (chunk) => {
-    data += chunk;
-  });
-  res.on('end', () => {
-    const doc = new JSDOM(data);
-    const reader = new Readability(doc.window.document);
-    const article = reader.parse();
-    article.content = [{ title: article.title, data: article.content }];
-    article.css = css;
-    const epub = new EPub(article, '/mnt/e/output.epub');
-    epub.render()
-      .then(() => {
-        console.log('Ebook Generated Successfully!');
-      })
-      .catch((err) => {
-        console.error('Failed to generate Ebook because of ', err);
-      });
-  });
+    let data = '';
+    res.on('data', (chunk) => {
+        data += chunk;
+    });
+    res.on('end', () => {
+        const doc = new JSDOM(data);
+        const reader = new Readability(doc.window.document);
+        const article = reader.parse();
+        article.content = [{ title: article.title, data: article.content }];
+        article.css = css;
+        const epub = new EPub(article, '/mnt/e/output.epub');
+        epub.render()
+            .then(() => {
+                console.log('Ebook Generated Successfully!');
+            })
+            .catch((err) => {
+                console.error('Failed to generate Ebook because of ', err);
+            });
+    });
 });
 request.on('error', (e) => {
-  console.log(e.message);
+    console.log(e.message);
 });
 request.end();
